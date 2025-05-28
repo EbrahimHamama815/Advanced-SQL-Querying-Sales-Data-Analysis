03cba60458f0cf4c9aef47362647dad1b02296b1
EbrahimHamama815 <ebrahim.hamama815@outlook.com>
2025-05-28 23:15:28 +0300

    Update 03 - Cumulative analysis.sql

--- Scripts/03 - Cumulative analysis.sql
+++ Scripts/03 - Cumulative analysis.sql
@@ -3,7 +3,7 @@ use datawarehouseanalytics;
 
 -- Calculating running total sales over the whole time period (months)
 select
-	order_date,
+    order_date,
     total_sales,
     sum(total_sales) over(order by order_date) as running_total_sales
 from
@@ -15,7 +15,7 @@ order by 1) t1;
 -- =============================================================================
 -- Calculating running total sales for each year separately
 select
-	order_date,
+    order_date,
     total_sales,
     sum(total_sales) over(partition by left(order_date,4) order by order_date) as running_total_sales
 from
@@ -27,7 +27,7 @@ order by 1) t1;
 -- =============================================================================
 -- Calculating moving average of price monthly
 select
-	order_date,
+    order_date,
     average_price,
     round(avg(average_price) over(order by order_date)) as moving_average_price
 from
