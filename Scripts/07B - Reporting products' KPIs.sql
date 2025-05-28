23905b4e1daaaa3591049f156a89fe47697e2850
EbrahimHamama815 <ebrahim.hamama815@outlook.com>
2025-05-28 23:20:23 +0300

    Update 07B - Reporting products' KPIs.sql

--- Scripts/07B - Reporting products' KPIs.sql
+++ Scripts/07B - Reporting products' KPIs.sql
@@ -28,7 +28,7 @@ create view gold_report_products as
 with base_query as
 -- Getting essential fields
 (select 
-	f.order_number,
+    f.order_number,
     f.order_date,
     f.customer_key,
     f.sales_amount,
@@ -45,12 +45,12 @@ with base_query as
  product_aggregation as
  -- Performing the necessary aggregations
  (select
-	product_key,
+    product_key,
     product_name,
     category,
     subcategory,
     cost,
-	timestampdiff(month,min(order_date),max(order_date)) as lifespan_in_months,
+    timestampdiff(month,min(order_date),max(order_date)) as lifespan_in_months,
     max(order_date) as last_sale_date,
     count(distinct order_number) as total_orders,
     count(distinct customer_key) as total_customers,
@@ -62,7 +62,7 @@ group by 1,2,3,4,5)
 
 -- Segmenting products by revenue, and calculating some important KPIs
 select 
-	product_key,
+    product_key,
     product_name,
     category,
     subcategory,
@@ -70,22 +70,22 @@ select
     last_sale_date,
     timestampdiff(month, last_sale_date, curdate()) as recency_in_months,
     case
-		when total_sales > 50000 then "High-Performer"
+	when total_sales > 50000 then "High-Performer"
         when total_sales >= 10000 then "Mid-Range"
         else "Low-Performer" 
-	end as product_segment,
+    end as product_segment,
     lifespan_in_months,
     total_orders,
     total_customers,
-	total_sales,
+    total_sales,
     total_quantity,
     avg_selling_price,
     case
-		when total_orders = 0 then 0
+	when total_orders = 0 then 0
         else total_sales/total_orders
-	end as avg_order_revenue,
+    end as avg_order_revenue,
     case
-		when lifespan_in_months = 0 then total_sales
+	when lifespan_in_months = 0 then total_sales
         else total_sales/lifespan_in_months
-	end as avg_monthly_revenue
+    end as avg_monthly_revenue
 from product_aggregation;
