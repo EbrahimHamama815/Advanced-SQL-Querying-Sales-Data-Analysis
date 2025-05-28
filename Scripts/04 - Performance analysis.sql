330a756ba0183edd89a553b63d60890980f1452e
EbrahimHamama815 <ebrahim.hamama815@outlook.com>
2025-05-28 23:16:59 +0300

    Update 04 - Performance analysis.sql

--- Scripts/04 - Performance analysis.sql
+++ Scripts/04 - Performance analysis.sql
@@ -4,7 +4,7 @@ use datawarehouseanalytics;
 -- Comparing yearly product sales to their average:
 with yearly_product_sales as
 (select 
-	year(f.order_date) as order_year,
+    year(f.order_date) as order_year,
     p.product_name,
     sum(f.sales_amount) as sales
 from gold_fact_sales f
@@ -13,13 +13,13 @@ where f.order_date is not null
 group by 1,2)
 
 select 
-	order_year,
+    order_year,
     product_name,
     sales,
     round(avg(sales) over(partition by product_name)) as avg_sales,
     sales-round(avg(sales) over(partition by product_name)) as diff_avg,
     case 
-		when sales-round(avg(sales) over(partition by product_name)) > 0 then "Above Average" 
+	when sales-round(avg(sales) over(partition by product_name)) > 0 then "Above Average" 
         when sales-round(avg(sales) over(partition by product_name)) = 0 then "Average"
         else "Below Average" 
 	end as avg_change
@@ -29,7 +29,7 @@ order by 2,1;
 -- Comparing yearly product sales to their previous year (Year-over-Year Analysis):
 with yearly_product_sales as
 (select 
-	year(f.order_date) as order_year,
+    year(f.order_date) as order_year,
     p.product_name,
     sum(f.sales_amount) as sales
 from gold_fact_sales f
@@ -38,13 +38,13 @@ where f.order_date is not null
 group by 1,2)
 
 select 
-	order_year,
+    order_year,
     product_name,
     sales,
     lag(sales) over(partition by product_name order by order_year) as prev_sales,
     sales-lag(sales) over(partition by product_name order by order_year) as diff_prev,
     case 
-		when sales-lag(sales) over(partition by product_name order by order_year) > 0 then "Increase" 
+	when sales-lag(sales) over(partition by product_name order by order_year) > 0 then "Increase" 
         when sales-lag(sales) over(partition by product_name order by order_year) < 0 then "Decrease"
         else "No Change" 
 	end as prev_year_change
