ca2e1b2338ce1abf98b567ba02748f39fe68a0c2
EbrahimHamama815 <ebrahim.hamama815@outlook.com>
2025-05-28 23:16:07 +0300

    Update 02 - Time series analysis.sql

--- Scripts/02 - Time series analysis.sql
+++ Scripts/02 - Time series analysis.sql
@@ -3,7 +3,7 @@ use datawarehouseanalytics;
 
  -- A yearly overview of sales
 select 
-	year(order_date) as order_year,
+    year(order_date) as order_year,
     sum(sales_amount) as total_sales,
     count(distinct customer_key) as total_customers,
     sum(quantity) as total_quantity_sold
@@ -14,7 +14,7 @@ order by 1;
 -- =============================================================================
  -- A monthly overview of sales to analyze seasonal behaviours
 select 
-	month(order_date) as order_month,
+    month(order_date) as order_month,
     sum(sales_amount) as total_sales,
     count(distinct customer_key) as total_customers,
     sum(quantity) as total_quantity_sold
@@ -25,7 +25,7 @@ order by 1;
 -- =============================================================================
 -- A month-year overview to track change over time
 select 
-	date_format(order_date,'%Y-%m') as order_month_year,
+    date_format(order_date,'%Y-%m') as order_month_year,
     sum(sales_amount) as total_sales,
     count(distinct customer_key) as total_customers,
     sum(quantity) as total_quantity_sold
